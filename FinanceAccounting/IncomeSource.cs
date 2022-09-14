506d37c6fb813dab08d7ca2af370405b29291ee9
kiespetch1 <90002199+kiespetch1@users.noreply.github.com>
2022-09-14 15:13:24 +0300

    Delete FinanceAccounting directory

--- FinanceAccounting/IncomeSource.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinanceAccounting;
-
-public class IncomeSource
-{
-    public int? Id { get; set; }
-    public string Name { get; set; }
-    public int UserId { get; set; }
-    public User User { get; set; }
-}
